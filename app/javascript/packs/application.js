f2f4cebcb83c13a8dffabae14a725549eb9be1d9
emmy <emmy66418@gmail.com>
2021-04-13 21:41:24 +0200

    Install bootstrap and modify index.html.erb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap/dist/js/bootstrap'
-import "bootstrap/dist/css/bootstrap";
 import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
