6ceb18e4d34b3991aafaeddd9eaee1750b3af66e
emmy <emmy66418@gmail.com>
2021-04-13 18:31:18 +0200

    import bootstrap inside applucation.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap";
 
 Rails.start()
 Turbolinks.start()
