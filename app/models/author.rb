ef31c8712f28e5d1ade656c1006087317a644450
emmy <emmy66418@gmail.com>
2021-04-17 11:00:13 +0200

    Validat email text field

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
   has_many :commentarys, dependent: :destroy
   has_many :post2s, dependent: :destroy
